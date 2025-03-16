680a6817ff4b1d391a82032e77ddab56fabdc04e
Ahdwka <ahed.wka4g@gmail.com>
2025-03-17 00:45:05 +0300

    ferafs

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import Contact from './pages/Contact';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import AdminDashboard from './pages/AdminDashboard';
-import NotFound from './pages/NotFound'; // Import the NotFound component
+import NotFound from './pages/NotFound';
 import { AuthProvider } from './contexts/AuthContext';
 
 function App() {
@@ -30,8 +30,6 @@ function App() {
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/admin" element={<AdminDashboard />} />
-
-                {/* Catch-all route for 404 errors */}
                 <Route path="*" element={<NotFound />} />
               </Routes>
             </main>
