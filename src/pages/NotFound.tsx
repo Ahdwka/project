3b781d492982a8d2c284f07384f3d7b306ed33d0
Ahdwka <ahed.wka4g@gmail.com>
2025-03-17 00:28:11 +0300

    ferafs

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,14 @@
-// src/pages/NotFound.js
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function NotFound() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+      <p className="mt-4 text-lg">The page you're looking for doesn't exist.</p>
+      <Link to="/" className="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+        Go Back Home
+      </Link>
     </div>
   );
 }
