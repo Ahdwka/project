a8d671a283d142e3c85fa77200899b4a9da96976
Ahdwka <ahed.wka4g@gmail.com>
2025-03-17 13:57:45 +0300

    kfvfiyf

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
@@ -23,48 +23,48 @@ const router = createBrowserRouter([
   { path: "/admin", element: <AdminDashboard />, errorElement: <NotFound /> },
 ]);
 
-export default function App() {
+// export default function App() {
+//   return (
+//     <AuthProvider>
+//       <div className="min-h-screen bg-gray-950 text-white">
+//         <Navbar />
+//         <div className="flex">
+//           <Sidebar />
+//           <main className="flex-1 p-6">
+//             <RouterProvider router={router} />
+//           </main>
+//         </div>
+//       </div>
+//     </AuthProvider>
+//   );
+// }
+// 
+function App() {
   return (
     <AuthProvider>
-      <div className="min-h-screen bg-gray-950 text-white">
-        <Navbar />
-        <div className="flex">
-          <Sidebar />
-          <main className="flex-1 p-6">
-            <RouterProvider router={router} />
-          </main>
+      <BrowserRouter>
+        <div className="min-h-screen bg-gray-950 text-white">
+          <Navbar />
+          <div className="flex">
+            <Sidebar />
+            <main className="flex-1 p-6">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/catalog" element={<Catalog />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/admin" element={<AdminDashboard />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </main>
+          </div>
+          <Toaster position="top-right" />
         </div>
-      </div>
+      </BrowserRouter>
     </AuthProvider>
   );
 }
 
-// function App() {
-//   return (
-//     <AuthProvider>
-//       <Router>
-//         <div className="min-h-screen bg-gray-950 text-white">
-//           <Navbar />
-//           <div className="flex">
-//             <Sidebar />
-//             <main className="flex-1 p-6">
-//               <Routes>
-//                 <Route path="/" element={<Home />} />
-//                 <Route path="/catalog" element={<Catalog />} />
-//                 <Route path="/about" element={<About />} />
-//                 <Route path="/contact" element={<Contact />} />
-//                 <Route path="/login" element={<Login />} />
-//                 <Route path="/register" element={<Register />} />
-//                 <Route path="/admin" element={<AdminDashboard />} />
-//                 <Route path="*" element={<NotFound />} />
-//               </Routes>
-//             </main>
-//           </div>
-//           <Toaster position="top-right" />
-//         </div>
-//       </Router>
-//     </AuthProvider>
-//   );
-// }
-
-// export default App;
+export default App;
